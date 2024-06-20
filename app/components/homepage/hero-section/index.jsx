baaac5501bb6dbc9b16e9f85e6b0829ea7e47d70
Vidhey <vidhey.bhogadi2003@gmail.com>
2024-06-21 01:08:46 +0530

    Update index.jsx

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -4,7 +4,7 @@ import { personalData } from "@/utils/data/personal-data";
 import Image from "next/image";
 import Link from "next/link";
 import { BsGithub, BsLinkedin } from "react-icons/bs";
-import { FaFacebook, FaTwitterSquare, FaDev } from "react-icons/fa";
+// import { FaFacebook, FaTwitterSquare, FaDev } from "react-icons/fa";
 import { MdDownload } from "react-icons/md";
 import { RiContactsFill } from "react-icons/ri";
 import { SiInstagram } from "react-icons/si";
