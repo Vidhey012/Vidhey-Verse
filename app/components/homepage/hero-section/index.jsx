c8440dd6bf61cd84b573d478d0df855859166482
Bhogadi Vidhey <vidhey.bhogadi2003@gmail.com>
2024-06-21 01:38:46 +0530

    First Commit

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -23,7 +23,7 @@ function HeroSection() {
       <div className="grid grid-cols-1 items-start lg:grid-cols-2 lg:gap-12 gap-y-8">
         <div className="order-2 lg:order-1 flex flex-col items-start justify-center p-2 pb-20 md:pb-10 lg:pt-10">
           <h1 className="text-3xl font-bold leading-10 text-white md:font-extrabold lg:text-[2.6rem] lg:leading-[3.5rem]">
-            Hello👋🏻, <br />
+            Hello 👋🏻, <br />
             This is {' '}
             <span className=" text-pink-500"><a href={personalData.github} target="_blank">{personalData.name}</a></span>
             {` , I'm a Professional `}
