6ba168c54954f3c75fbba47c2544f3e5bf898476
Bhogadi Vidhey <vidhey.bhogadi2003@gmail.com>
2024-06-21 16:13:44 +0530

    Second Commit

--- app/blog/page.js
+++ app/blog/page.js
@@ -22,8 +22,8 @@ async function page() {
       <div className="flex justify-center my-5 lg:py-8">
         <div className="flex  items-center">
           <span className="w-24 h-[2px] bg-[#1a1443]"></span>
-          <span className="bg-[#1a1443] w-fit text-white p-2 px-5 text-2xl rounded-md">
-            All Certifications
+          <span className="bg-[#1a1443] w-fit text-white text-center p-2 px-5 text-2xl rounded-md">
+            All Technical Certifications
           </span>
           <span className="w-24 h-[2px] bg-[#1a1443]"></span>
         </div>
