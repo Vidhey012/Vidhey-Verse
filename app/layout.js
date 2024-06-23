31c5fa4999d5f599c6938ce8cc0e3fff95d0130c
Bhogadi Vidhey <vidhey.bhogadi2003@gmail.com>
2024-06-24 01:03:04 +0530

    Fourth Commit

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Footer from './components/footer';
 import Navbar from './components/navbar';
+import AudioPlayer from './components/AudioPlayer';
 import './css/card.scss';
 import './css/globals.scss';
 const inter = Inter({ subsets: ['latin'] });
@@ -16,6 +17,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>𝓥𝓲𝓭𝓱𝓮𝔂 𝓑𝓱𝓸𝓰𝓪𝓭𝓲</title>
+        <link rel="icon" type="image/x-icon" href="title_logo.ico"/>
+      </head>
       <body className={inter.className}>
         <ToastContainer />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
@@ -23,6 +28,7 @@ export default function RootLayout({ children }) {
           {children}
         </main>
         <Footer />
+        <AudioPlayer />
       </body>
       <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} />
     </html>
